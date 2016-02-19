910cbfac6cea2070f60a6a27545d0a294665d9ee
zhewuqw <zhewu0804@gmail.com>
2016-02-19 00:12:37 -0500

    first commit

--- src/main/java/test/Sandbox.java
+++ src/main/java/test/Sandbox.java
@@ -4,63 +4,22 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-@Path("sandbox")
+ 
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("myresource")
 public class Sandbox {
-	ObjectMapper mapper = new ObjectMapper();
-
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("person")
-	public Response getIt() {
-		try {
-			Person gula = new Person("Gula", "Nurmatova", "CT");
-			String strGula = mapper.writeValueAsString(gula);
-			return Response.status(Response.Status.OK)
-					.entity(strGula).build();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return Response.status(Response.Status.BAD_REQUEST)
-					.entity("Could not process your request").build();
-		}
-	}
-}
-
-class Person {
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLocation() {
-		return location;
-	}
-
-	public void setLocation(String location) {
-		this.location = location;
-	}
-
-	String lastName;
-	String firstName;
-	String location;
-
-	public Person(String firstName, String lastName, String location) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.location = location;
-	}
+ 
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
+    }
 }
